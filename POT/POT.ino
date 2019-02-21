786268508b95e947fc085d521c50ac63d65ac42c
Calbetense <daniel.calbet.campillo@alumnos.upm.es>
2019-02-21 23:22:37 +0100

    Con esto funciona en el arduino UNO

--- POT/POT.ino
+++ POT/POT.ino
@@ -4,15 +4,13 @@
 
 void setup() {
   pinMode(LedPin, OUTPUT);
-  Serial.begin(9600);
-
- 
+  Serial.begin(9600); //Con esto podemos ver con el monitor serie el valor real del potenciómetro (0 - 1024)
 }
 
 void loop() {
   int val = analogRead(PotPin);
-  analogWrite(LedPin, val/4);
+  analogWrite(LedPin, val/4);   //La diferencia entre el valor de lectura (1024) y escritura (255) es aproximádamente de 4
 
   Serial.println(val); 
-  delay(5);
+  delay(5); //No sé si realmente es necesario, pero bueno, por costumbre
 }
